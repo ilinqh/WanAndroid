141ef62aabc9270250290211c2d8e0098d16a6b1
林钦宏 <649995267@qq.com>
2019-06-28 10:51:29 +0800

    接入腾讯 X5 内核

--- app/src/main/java/lqh/wanandroid/MainActivity.kt
+++ app/src/main/java/lqh/wanandroid/MainActivity.kt
@@ -1,9 +1,11 @@
 package lqh.wanandroid
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import lqh.kframe.util.ScreenUtils
+import lqh.kframe.util.dp2px
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button.setOnClickListener {
-            imageView.radius = ScreenUtils.dp2px(this, 32).toFloat()
+            imageView.radius = 32.dp2px().toFloat()
         }
+        button.setBackgroundColor(Color.RED)
+        button.setBackgroundResource(R.color.colorAccent)
+
+        var info = packageManager.getPackageInfo(this.packageName, 0)
+        var name1 = info.packageName
+        Log.e("NAME1", "name1 -> $name1")
+
+        var name2 = packageName
+        Log.e("NAME2", "name2 -> $name2")
     }
 }
---
+++ kframe/src/main/java/lqh/kframe/util/NetworkUtils.kt
@@ -0,0 +1,48 @@
+package lqh.kframe.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+/**
+ * 功能：获取网络状态的工具类
+ * -------------------------------------------------------------------------------------------------
+ * 创建者：@author 林钦宏
+ * -------------------------------------------------------------------------------------------------
+ * 创建日期：2019/6/26
+ * -------------------------------------------------------------------------------------------------
+ * 更新历史
+ * 编号|更新日期|更新人|更新内容
+ */
+class NetworkUtils {
+
+    companion object {
+        /**
+         * 判断当前是否有网络连接
+         *
+         * @param context context
+         * @return 网络连接状态
+         */
+        fun isNetworkConnext(context: Context): Boolean {
+            val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networkInfo = manager.activeNetworkInfo
+            return networkInfo?.isAvailable == true
+        }
+
+        /**
+         * 判断当前是否使用手机数据流量网络
+         *
+         * @param context context
+         * @return isMobileConnect
+         */
+        fun isMobileConnect(context: Context): Boolean {
+            val manager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val networks = manager.allNetworks
+            for (network in networks) {
+
+            }
+            val networkInfo = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+            return false
+        }
+    }
+
+}
